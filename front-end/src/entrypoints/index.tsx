36d55539779692b5098ccf960558d09acc6a3367
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2024-11-03 18:47:08 -0700

    fix: comments

--- front-end/src/entrypoints/index.tsx
+++ front-end/src/entrypoints/index.tsx
@@ -4,7 +4,7 @@ import { App } from "@/components/app";
 
 const container = document.querySelector("#root");
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- we control the html, the #root will be there
 const root = createRoot(container!);
 
 root.render(<App />);
