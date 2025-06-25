4ea0248c595bcc9f4f666d8c0b4e1ed30b01082c
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-06-25 07:46:14 -0700

    fix: remove @eslint-compat, it's unused

--- front-end/src/types/environment.ts
+++ front-end/src/types/environment.ts
@@ -1,4 +1,6 @@
-/* eslint-disable unicorn/no-empty-file -- Nothing here yet */ // type ImportMetaEnv = { readonly [P in keyof Config]: Config[P] };
+/* eslint-disable unicorn/no-empty-file -- Nothing here yet */
+
+// type ImportMetaEnv = { readonly [P in keyof Config]: Config[P] };
 
 // interface ImportMeta {
 //     readonly env: ImportMetaEnv;
