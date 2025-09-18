85257f73c34a13c00bbcb1a17d1a8dad9a70c7d2
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-09-18 09:17:17 -0700

    fix: clippy 1.90 fixes

--- back-end/src/signal_handlers.rs
+++ back-end/src/signal_handlers.rs
@@ -1,9 +1,22 @@
 use tokio::signal::unix::{SignalKind, signal};
 
+/// Waits forever for a SIGTERM
+pub async fn wait_for_sigterm() -> Result<(), std::io::Error> {
+    signal(SignalKind::terminate())?.recv().await;
+
+    Ok(())
+}
+
 /// Waits forever for a SIGUSR1
-pub async fn wait_for_sigusr1() -> Option<()> {
-    signal(SignalKind::user_defined1())
-        .expect("Failed to register SIGUSR1 handler")
-        .recv()
-        .await
+pub async fn wait_for_sigusr1() -> Result<(), std::io::Error> {
+    signal(SignalKind::user_defined1())?.recv().await;
+
+    Ok(())
+}
+
+/// Waits forever for a SIGINT
+pub async fn wait_for_sigint() -> Result<(), std::io::Error> {
+    tokio::signal::ctrl_c().await?;
+
+    Ok(())
 }
