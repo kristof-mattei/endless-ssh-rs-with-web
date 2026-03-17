a8b7ee2b27e83dbf671da3894a7cc761dc144874
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-03-17 04:14:37 +0000

    chore: use variable to prevent quotes from messing up command

--- init-db/init-user-db.sh
+++ init-db/init-user-db.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE USER "endless-ssh-rs" WITH PASSWORD '$POSTGRES_PASSWORD';
+psql --set=ON_ERROR_STOP=1 --set=postgres_password="$POSTGRES_PASSWORD" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER "endless-ssh-rs" WITH PASSWORD :'postgres_password';
   CREATE DATABASE "endless-ssh-rs" WITH OWNER "endless-ssh-rs";
   -- GRANT ALL PRIVILEGES ON DATABASE "endless-ssh-rs" TO "endless-ssh-rs";
 EOSQL
