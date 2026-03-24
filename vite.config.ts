e3c2203d275e97b9bfea0e0559208edb621aff8a
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-03-24 12:13:01 -0700

    chore: remove missed polyfill inclusion

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ const configFunction: ViteUserConfigFn = defineConfig(({ mode }) => {
         ],
         optimizeDeps: {
             noDiscovery: true,
-            include: ["@js-temporal/polyfill", "ip-address", "maplibre-gl", "react-dom/client", "recharts"],
+            include: ["ip-address", "maplibre-gl", "react-dom/client", "recharts"],
             // exclude: ["src/entrypoints/index.ts"],
         },
         root: "front-end/src",
